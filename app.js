3e3aac9c0cef2b2dd2b3a1a7ca6051c4b70ab092
Prateek Darshan <prateekdarshan@gmail.com>
2023-07-16 18:30:47 +0530

    working build v1: (16/07)

--- app.js
+++ app.js
@@ -20,41 +20,72 @@ mongoose.connect(
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
-const imageSchema = new mongoose.Schema({
-  name: String,
-  image: {
-    data: Buffer,
-    contentType: String,
+const bookSchema = new mongoose.Schema({
+  book_name: String,
+  book_author: String,
+  book_cover_address: String,
+  book_address: String,
+});
+
+const Book = new mongoose.model("Book", bookSchema);
+
+// const storage = multer.memoryStorage()
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    if (file.fieldname === "book_cover") {
+      cb(null, "./public/book_covers");
+    }
+    if (file.fieldname === "book") {
+      cb(null, "./public/books");
+    }
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${file.originalname}`);
   },
 });
 
-const Image = new mongoose.model("Image", imageSchema);
-
-const storage = multer.memoryStorage();
-
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
 app.get("/admin", (req, res) => {
   res.render("admin");
 });
 
-app.post("/upload", upload.single("book"), (req, res) => {
-  const images = new Image({
-    name: req.file.originalname,
-    image: {
-      data: req.file.buffer,
-      contentType: req.file.mimetype,
-    },
-  });
-  images.save();
+app.post(
+  "/upload",
+  upload.fields([
+    { name: "book_cover", maxCount: 1 },
+    { name: "book", maxCount: 1 },
+  ]),
+  (req, res) => {
+    const books = new Book({
+      book_name: req.body.name,
+      book_author: req.body.author,
+      book_cover_address: "./book_covers/" + req.body.name + ".png",
+      book_address: req.body.name + ".pdf",
+    });
 
-  res.send("done");
-});
+    books.save();
+    return res.redirect("/");
+
+    // res.send("done");
+  }
+);
 
 app.get("/", (req, res) => {
-  Image.find().then((allImages) => {
+  Book.find({}).then((allBooks) => {
     res.render("index", {
-      images: allImages,
+      allBooks: allBooks,
+    });
+  });
+});
+
+app.get("/books/:book_name", (req, res) => {
+  console.log(req.body);
+  Book.findOne({ book_name: req.params.book_name }).then((foundBook) => {
+    res.render("book", {
+      book_name: foundBook.book_name,
+      book_address: foundBook.book_address,
     });
   });
 });
